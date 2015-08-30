8f5a8d10514927b04a1f388de18b75f9f4cc74d8
user <securitycloud@sc6.ro.vutbr.cz>
2015-08-30 08:32:24 +0200

    sleep for topics

--- reset_kafka_topics.sh
+++ reset_kafka_topics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /home/securitycloud/kafka/kafka_2.9.2-0.8.2.1/bin/kafka-topics.sh --zookeeper sc7:2181 --delete --topic tst
 /home/securitycloud/kafka/kafka_2.9.2-0.8.2.1/bin/kafka-topics.sh --zookeeper sc7:2181 --delete --topic out
+sleep 10
 /home/securitycloud/kafka/kafka_2.9.2-0.8.2.1/bin/kafka-topics.sh --zookeeper sc7:2181 --create --topic tst --partitions 100 --replication-factor 2
 /home/securitycloud/kafka/kafka_2.9.2-0.8.2.1/bin/kafka-topics.sh --zookeeper sc7:2181 --create --topic out --partitions 1 --replication-factor 2
