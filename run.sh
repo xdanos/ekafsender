98b7b778122a1105e400614becea15b82c674a68
xtovarn <xtovarn@fi.muni.cz>
2015-09-01 18:56:15 +0200

    * config

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl -name node -s ekafsender_app -pa ebin/ deps/*/ebin/ -noshell +A 128
+erl -name node -s ekafsender_app -pa ebin/ deps/*/ebin/ -noshell +A 32 -config config/ekafsender.config
